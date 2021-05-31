17b20569835e5495870683620128fbc51cb6bfef
unknown <olena.denysova88@gmail.com>
2021-05-31 14:10:48 +0200

    deployment

---
+++ js/script.js
@@ -0,0 +1,354 @@
+$(document).ready(function () {
+  //SLIDER//
+  const slides = document.querySelectorAll(".offer__slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current");
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach(item => (item.style.display = "none"));
+
+    slides[slideIndex - 1].style.display = "block";
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener("click", () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", () => {
+    plusSlides(1);
+  });
+
+  //switch sliders with keyboard
+  function eventKey(e) {
+    switch (e.keyCode) {
+      case 37:
+        plusSlides(-1)
+        break;
+
+      case 39:
+        plusSlides(1)
+        break;
+    }
+  }
+  addEventListener("keydown", eventKey);
+
+  //TIMER//
+  const deadline = "2021-08-20";
+
+  function getTimeRemaining(endtime) {
+    //miliseconds left
+    const timeLeft = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    //% -modulo to leave only necessary value
+    const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+    const seconds = Math.floor((timeLeft / 1000) % 60);
+
+    return {
+      total: timeLeft,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
+
+  //to receive format with 0
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days");
+    const hours = timer.querySelector("#hours");
+    const minutes = timer.querySelector("#minutes");
+    const seconds = timer.querySelector("#seconds");
+
+    const timeInterval = setInterval(updateClock, 1000);
+
+    //to fix bug with blinking timer
+    updateClock();
+
+    function updateClock() {
+      const timeLeftNow = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(timeLeftNow.days);
+      hours.innerHTML = getZero(timeLeftNow.hours);
+      minutes.innerHTML = getZero(timeLeftNow.minutes);
+      seconds.innerHTML = getZero(timeLeftNow.seconds);
+
+      //to stop counting after deadline
+      if (timeLeftNow <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", deadline);
+
+  //TABS//
+
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  //CALCULATOR//
+  const result = document.querySelector(".calculating__result span");
+
+  let sex, height, weight, age, ratio;
+
+  //check if in local storage is saved user's choice if no, set default values
+  if (localStorage.getItem("sex")) {
+    sex = localStorage.getItem("sex");
+  } else {
+    sex = "female";
+    localStorage.setItem("sex", "female");
+  }
+
+  if (localStorage.getItem("ratio")) {
+    ratio = localStorage.getItem("ratio");
+  } else {
+    ratio = 1.375;
+    localStorage.setItem("ratio", 1.375);
+  }
+
+  //class activity for local storage
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+      if (elem.getAttribute("id") === localStorage.getItem("sex")) {
+        elem.classList.add(activeClass);
+      }
+      if (elem.getAttribute("data-ratio") === localStorage.getItem("ratio")) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+
+  initLocalSettings("#gender div", "calculating__choose-item_active");
+  initLocalSettings(
+    ".calculating__choose_big div",
+    "calculating__choose-item_active"
+  );
+
+  function calcTotal() {
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = "____";
+      return;
+    }
+    if (sex === "female") {
+      result.textContent = Math.round(
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+      );
+    } else {
+      result.textContent = Math.round(
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+      );
+    }
+  }
+  calcTotal();
+
+  function getStaticInformation(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(elem => {
+      elem.addEventListener("click", e => {
+        if (e.target.getAttribute("data-ratio")) {
+          ratio = +e.target.getAttribute("data-ratio");
+          localStorage.setItem("ratio", +e.target.getAttribute("data-ratio"));
+        } else {
+          sex = e.target.getAttribute("id");
+          localStorage.setItem("sex", e.target.getAttribute("id"));
+        }
+
+        elements.forEach(elem => {
+          elem.classList.remove(activeClass);
+        });
+
+        e.target.classList.add(activeClass);
+
+        calcTotal();
+      });
+    });
+  }
+  getStaticInformation("#gender div", "calculating__choose-item_active");
+  getStaticInformation(
+    ".calculating__choose_big div",
+    "calculating__choose-item_active"
+  );
+
+  function getDynamicInformation(selector) {
+    const input = document.querySelector(selector);
+
+    input.addEventListener("input", () => {
+      //if in input not numbers
+      if (input.value.match(/\D/g)) {
+        input.style.border = "1px solid red";
+      } else {
+        input.style.border = "none";
+      }
+
+      switch (input.getAttribute("id")) {
+        case "height":
+          height = +input.value;
+          break;
+        case "weight":
+          weight = +input.value;
+          break;
+        case "age":
+          age = +input.value;
+          break;
+      }
+      calcTotal();
+    });
+  }
+
+  getDynamicInformation("#height");
+  getDynamicInformation("#weight");
+  getDynamicInformation("#age");
+
+  // SCROLL TO THE TOP//
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
+
+  $("a[href^='#up']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(_href).offset().top + "px"
+    });
+    return false;
+  });
+
+  // MODAL //
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+
+  // FORM VALIDATION //
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+          maxlength: 25
+        },
+        phone: {
+          required: true,
+          maxlength: 25
+        },
+        email: {
+          required: true,
+          email: true,
+          maxlength: 35
+        }
+      },
+      messages: {
+        name: {
+          required: "Please, enter your name",
+          minlength: jQuery.validator.format("Enter at least {0} characters!"),
+          maxlength: jQuery.validator.format(
+            "Please, no more than {0} characters!"
+          )
+        },
+        phone: {
+          required: "Please, enter your Phone number"
+        },
+        email: {
+          required: "Please, enter your email",
+          email: "Email is not valid"
+        }
+      }
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
+
+  //CLEAR FORM AFTER SUBMIT//
+  window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+      form.reset();
+    }
+  };
+});
